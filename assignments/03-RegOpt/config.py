0f80b21010d6f5da7e6862e490033ba4666f5249
haoershi <haoershi@seas.upenn.edu>
2023-02-15 20:39:44 -0500

    try

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,9 +8,9 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
     """_summary_"""
 
-    batch_size = 64
+    batch_size = 32
     num_epochs = 12
-    initial_learning_rate = 0.0014
+    initial_learning_rate = 0.0011
     initial_weight_decay = 0
 
     lrs_kwargs = {
@@ -21,9 +21,9 @@ class CONFIG:
         # 'step_size': 100,
         # "gamma": 0.9,
         # "milestones": [3000, 3600, 4000, 4800, 5400, 6000],
-        "T_0": 10,
-        "eta_min": 0.0005,
-        "T_mult": 5,
+        "T_0": 32,
+        "eta_min": 0.00035,
+        "T_mult": 32,
     }  # gamma=0.9, milestones=[30,80]
 
     optimizer_factory: Callable[
@@ -32,7 +32,7 @@ class CONFIG:
         model.parameters(),
         lr=CONFIG.initial_learning_rate,
         weight_decay=CONFIG.initial_weight_decay,
-        amsgrad=False,
+        amsgrad=True,
     )  # lr=0.001, betas=(0.9, 0.999), weight_decay=0, amsgrad=False
 
     transforms = Compose(
