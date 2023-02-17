03c38dd0b2f3287e82174dd550efd7e210a8dafa
haoershi <haoershi@seas.upenn.edu>
2023-02-17 00:30:47 -0500

    test shrink both

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -17,7 +17,7 @@ class CONFIG:
 
     batch_size = 32
     num_epochs = 12
-    initial_learning_rate = 0.002
+    initial_learning_rate = 0.0018
     initial_weight_decay = 0
 
     lrs_kwargs = {
@@ -28,9 +28,9 @@ class CONFIG:
         # 'step_size': 100,
         "gamma": 0.95,
         # "milestones": [3000, 3600, 4000, 4800, 5400, 6000],
-        "T_0": 10,
-        "eta_min": 0.0003,
-        "T_mult": 16,
+        "T_0": 32,
+        "eta_min": 0.0005,
+        "T_mult": 32,
     }  # gamma=0.9, milestones=[30,80]
 
     optimizer_factory: Callable[
