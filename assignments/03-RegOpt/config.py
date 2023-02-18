6326a4e962760e7b35082c181bbc29e95536b65a
haoershi <haoershi@seas.upenn.edu>
2023-02-18 01:56:07 -0500

    gamma0.9

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -16,8 +16,8 @@ class CONFIG:
     """_summary_"""
 
     batch_size = 32
-    num_epochs = 10
-    initial_learning_rate = 0.0015
+    num_epochs = 12
+    initial_learning_rate = 0.0018
     initial_weight_decay = 0
 
     lrs_kwargs = {
@@ -26,7 +26,7 @@ class CONFIG:
         # 'lr_lambda': [lambda epoch: 0.999],
         # 'start_epoch': 20,
         # 'step_size': 100,
-        "gamma": 0.95,
+        "gamma": 0.9,
         # "milestones": [3000, 3600, 4000, 4800, 5400, 6000],
         "T_0": 32,
         "eta_min": 0.00035,
