bbab310bd02f7fe18cd45d612ee103a53c52faaf
haoershi <haoershi@seas.upenn.edu>
2023-03-14 23:57:12 -0400

    test

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -21,15 +21,16 @@ class Model(torch.nn.Module):
 
         # self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, stride=1, padding=1)
         # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.ker = 5
+        self.ker = 3
         self.pad = (self.ker - 1) // 2
         self.nchan = 16
+        # self.bias = BiasLayer((num_channels, 32, 32))
         self.conv2 = nn.Conv2d(
-            num_channels, self.nchan, kernel_size=self.ker, stride=2, padding=self.pad
+            num_channels, self.nchan, kernel_size=self.ker, stride=1, padding=self.pad
         )
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(self.nchan * 8 * 8, num_classes)
+        self.fc1 = nn.Linear(self.nchan * 16 * 16, num_classes)
         # self.fc2 = nn.Linear(256, 100)
         # self.fc3 = nn.Linear(100, num_classes)
         nn.init.xavier_uniform_(self.conv2.weight)
@@ -46,15 +47,12 @@ class Model(torch.nn.Module):
         Returns:
             torch.Tensor: _description_
         """
-        # x = self.conv1(x)
-        # x = F.relu(x)
-        # x = self.pool1(x)
 
         x = self.conv2(x)
         x = F.relu(x)
         x = self.pool2(x)
 
-        x = x.view(-1, self.nchan * 8 * 8)
+        x = x.view(-1, self.nchan * 16 * 16)
         # x = F.relu(self.fc1(x))
         # x = F.relu(self.fc2(x))
         x = self.fc1(x)
