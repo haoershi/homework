f1f1b8e2a6e438d606ece7e7f33e57e6aa7f0c9c
haoershi <haoershi@seas.upenn.edu>
2023-03-17 15:54:21 -0400

    test

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,32 +2,15 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import (
-    Compose,
-    ToTensor,
-    # Normalize,
-    # Resize,
-    # RandomCrop,
-    # RandomHorizontalFlip,
-)
+from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 35  # 200
+    batch_size = 35
     num_epochs = 8
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=2e-3)
 
-    transforms = Compose(
-        [
-            # RandomCrop(
-            #     16, padding=4
-            # ),  # randomly crop the image to 32x32 with padding of 4 pixels
-            # RandomHorizontalFlip(),  # randomly flip the image horizontally
-            # Resize(28),
-            ToTensor(),
-            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ]
-    )
+    transforms = Compose([ToTensor()])
