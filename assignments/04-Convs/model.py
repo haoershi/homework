4cc6993b6a956ed289947d575d34425ae57bcbc8
haoershi <haoershi@seas.upenn.edu>
2023-03-17 14:06:20 -0400

    24/3

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -4,18 +4,17 @@ import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
-    """_summary_
-
-    Args:
-        torch (_type_): _description_
+    """
+    A simple CNN model that reaches 0.55 accuracy that cost less than 15 sec for an epoch, with batch size 200 
+    and learning rate 6e-3 on CIFAR10 dataset
     """
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        """_summary_
-
+        """
+        Structure of the model that runs as fast as possible
         Args:
-            num_channels (int): _description_
-            num_classes (int): _description_
+            num_channels (int): number of channels in the input image
+            num_classes (int): number of classes in the dataset
         """
         super(Model, self).__init__()
 
@@ -23,36 +22,44 @@ class Model(torch.nn.Module):
         # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.ker = 3
         self.pad = (self.ker - 1) // 2
-        self.nchan = 16
-        # self.bias = BiasLayer((num_channels, 32, 32))
+        self.nchan = 24
         self.conv2 = nn.Conv2d(
-            num_channels, self.nchan, kernel_size=self.ker, stride=1, padding=self.pad
+            num_channels,
+            self.nchan,
+            kernel_size=self.ker,
+            stride=2,
+            padding=self.pad,
         )
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(self.nchan * 16 * 16, num_classes)
+        self.fc1 = nn.Linear(self.nchan * 8 * 8, num_classes)
         # self.fc2 = nn.Linear(256, 100)
         # self.fc3 = nn.Linear(100, num_classes)
         nn.init.xavier_uniform_(self.conv2.weight)
         nn.init.xavier_uniform_(self.fc1.weight)
         # nn.init.xavier_uniform_(self.fc2.weight)
         # nn.init.xavier_uniform_(self.fc3.weight)
+        # self.convdepth = nn.Conv2d(num_channels,self.nchan,kernel_size = 1)
+        # nn.init.xavier_uniform_(self.convdepth.weight)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """_summary_
-
-        Args:
-            x (torch.Tensor): _description_
-
-        Returns:
-            torch.Tensor: _description_
         """
+        Forward pass of the model
+        Args:
+            x (torch.Tensor): input image
+        Returns:
+            torch.Tensor: output logits
+        """
+        # x = self.conv1(x)
+        # x = F.relu(x)
+        # x = self.pool1(x)
 
         x = self.conv2(x)
+        # x = self.convdepth(x)
         x = F.relu(x)
         x = self.pool2(x)
 
-        x = x.view(-1, self.nchan * 16 * 16)
+        x = x.view(-1, self.nchan * 8 * 8)
         # x = F.relu(self.fc1(x))
         # x = F.relu(self.fc2(x))
         x = self.fc1(x)
