7e5ea191d3f7d0c7522bee300032ae0b83f90a8d
haoershi <haoershi@seas.upenn.edu>
2023-03-17 14:55:35 -0400

    test

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -24,8 +24,8 @@ class Model(torch.nn.Module):
         self.ker = 3
         self.pad = (self.ker - 1) // 2
         self.nchan = 16
-        self.conv2 = nn.LazyConv2d(
-            # num_channels,
+        self.conv2 = nn.Conv2d(
+            num_channels,
             self.nchan,
             kernel_size=self.ker,
             stride=1,
@@ -36,8 +36,8 @@ class Model(torch.nn.Module):
         self.fc1 = nn.Linear(self.nchan * 10 * 10, num_classes)
         # self.fc2 = nn.Linear(256, 100)
         # self.fc3 = nn.Linear(100, num_classes)
-        # nn.init.xavier_uniform_(self.conv2.weight)
-        # nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.fc1.weight)
         # nn.init.xavier_uniform_(self.fc2.weight)
         # nn.init.xavier_uniform_(self.fc3.weight)
         # self.convdepth = nn.Conv2d(num_channels,self.nchan,kernel_size = 1)
@@ -45,16 +45,16 @@ class Model(torch.nn.Module):
         self.model = nn.Sequential(
             self.conv2, nn.ReLU(), self.pool2, nn.Flatten(), self.fc1
         )
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=2e-3)
-        criterion = nn.CrossEntropyLoss()
-        for _ in range(10):
-            x = torch.randn(200, 3, 32, 32)
-            y = torch.randn(200, 10)
-            y_pred = self.model(x)
-            loss = criterion(y_pred, y)
-            loss.backward()
-            # optimizer.step()
-            optimizer.zero_grad()
+        # optimizer = torch.optim.Adam(self.model.parameters(), lr=2e-3)
+        # criterion = nn.CrossEntropyLoss()
+        # for _ in range(10):
+        #     x = torch.randn(200, 3, 32, 32)
+        #     y = torch.randn(200, 10)
+        #     y_pred = self.model(x)
+        #     loss = criterion(y_pred, y)
+        #     loss.backward()
+        #     # optimizer.step()
+        #     optimizer.zero_grad()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
